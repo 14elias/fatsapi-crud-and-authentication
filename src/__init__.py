e25af5aed617f95003e929a1058b3ffe39224746
14elias <ellamebrahtom1995@gmail.com>
2025-10-03 20:07:05 +0300

    updated

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from src.book.route import book_router
 from src.book.data import init_db
 from src.auth.utility import user_router
 from src.post.route import post_router
@@ -13,7 +12,6 @@ app = FastAPI(
 
 
 
-app.include_router(book_router, prefix=f'/api/{version}/books')
 app.include_router(user_router, prefix=f'/api/{version}/users')
 app.include_router(post_router)
 
